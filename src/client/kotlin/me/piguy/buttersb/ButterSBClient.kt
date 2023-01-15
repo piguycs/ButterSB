09ab4302fd325ce4f9c3ff19340ea9d62e6b621c
thepiguy <kunal.a.dandekar@gmail.com>
2023-01-15 17:16:18 +0100

    Initial files

---
+++ src/client/kotlin/me/piguy/buttersb/ButterSBClient.kt
@@ -0,0 +1,9 @@
+package me.piguy.buttersb
+
+import net.fabricmc.api.ClientModInitializer
+
+object ButterSBClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
